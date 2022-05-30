3efb7784e3913a0d8a398112d7f0ad321abb5d0d
Mohammad Shakir <65658120+Shaquib47@users.noreply.github.com>
2022-05-30 21:34:14 +0530

    initial commit, created recipe spring boot application

---
+++ src/main/java/com/ccube/recipe/RecipeApplication.java
@@ -0,0 +1,13 @@
+package com.ccube.recipe;
+
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+
+@SpringBootApplication
+public class RecipeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecipeApplication.class, args);
+    }
+
+}
